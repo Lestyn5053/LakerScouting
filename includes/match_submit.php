718c96234447b5be3dffa37d4a9fc2e85c1b1bc2
Lestyn Brooks <wiiman98989@gmail.com>
2022-03-01 15:08:56 -0500

    Updated Match Submission for 2022 game

--- includes/match_submit.php
+++ includes/match_submit.php
@@ -8,7 +8,6 @@
         $ScoutName = $_POST['name'];
         $TeamNum = $_POST['team'];
         $MatchNum = $_POST['match'];
-        $Preload = $_POST['preload'];
         $initLine = $_POST['auto_move'];
         $autoHGScored = $_POST['a_hg_scored'];
         $autoHGMissed = $_POST['a_hg_missed'];
@@ -19,19 +18,14 @@
         $teleHGScored = $_POST['hg_scored'];
         $teleHGMissed = $_POST['hg_missed'];
         $teleHGTotal = $teleHGScored + $teleHGMissed;
-        $trenchHGScored = $_POST['hg_scored_trench'];
-        $trenchHGMissed = $_POST['hg_missed_trench'];
-        $trenchHGTotal = $trenchHGScored + $trenchHGMissed;
+        $launchpadHGScored = $_POST['hg_scored_launch_pad'];
+        $launchpadHGMissed = $_POST['hg_missed_launch_pad'];
+        $launchpadHGTotal = $launchpadHGScored + $launchpadHGMissed;
         $teleLGScored = $_POST['lg_scored'];
         $teleLGMissed = $_POST['lg_missed'];
         $teleLGTotal = $teleLGScored + $teleLGMissed;
         $BallsDropped = $_POST['balls_dropped'];
-        $RotationControl = $_POST['rotation_control'];
-        $RCFIRST = $_POST['rc_accurate'];
-        $PositionControl = $_POST['position_control'];
-        $PCFIRST = $_POST['pc_accurate'];
         $Climb = $_POST['climb'];
-        $Level = $_POST['level'];
         $DriveTeam = $_POST['drive_team'];
         $Defense = $_POST['defense'];
         $DefenseComments = $_POST['defense_comments'];
@@ -40,8 +34,8 @@
         $Penalties = $_POST['penalties'];
         $Comments = $_POST['comments'];
 
-        $sql = "INSERT INTO MatchStats VALUES(NULL, $TeamNum, $CompID, $MatchNum, '$ScoutName', $Preload, $initLine, $autoHGScored, $autoHGMissed, $autoHGTotal, $autoLGScored, $autoLGMissed, $autoLGTotal,
-        $teleHGScored, $teleHGMissed, $teleHGTotal, $trenchHGScored, $trenchHGMissed, $trenchHGTotal, $teleLGScored, $teleLGMissed, $teleLGTotal, $BallsDropped, $RotationControl, $RCFIRST, $PositionControl, $PCFIRST, '$Climb', $Level, $DriveTeam, $Defense,
+        $sql = "INSERT INTO MatchStats VALUES(NULL, $TeamNum, $CompID, $MatchNum, '$ScoutName', $initLine, $autoHGScored, $autoHGMissed, $autoHGTotal, $autoLGScored, $autoLGMissed, $autoLGTotal,
+        $teleHGScored, $teleHGMissed, $teleHGTotal, $launchpadHGScored, $launchpadHGMissed, $launchpadHGTotal, $teleLGScored, $teleLGMissed, $teleLGTotal, $BallsDropped, '$Climb', $DriveTeam, $Defense,
         '$DefenseComments', $DefenseRating, $IntakeRating, '$Penalties', '$Comments')";
         if(mysqli_query($conn, $sql))
         {
