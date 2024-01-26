d2fd8618bad01563206f1eda813a459bf4cdab5b
Lestyn Brooks <wiiman98989@gmail.com>
2024-01-25 21:47:31 -0500

    Rewrote match submit script

--- includes/match_submit.php
+++ includes/match_submit.php
@@ -1,33 +1,35 @@
 <?php
-    if(isset($_POST['match-submit']))
-    {
+    if(isset($_POST['match-submit'])) {
         require 'dbh.php';
 
         $CompID = trim($_GET['CompID']);
-        //Declaring variables for the values collected by the scout
+        //Basic Info
         $ScoutName = $_POST['name'];
         $TeamNum = $_POST['team'];
         $MatchNum = $_POST['match'];
-        $startingZone = $_POST['auto_move'];
-        $autoSpeakerScored = $_POST['a_speaker_scored'];
-        $autoSpeakerMissed = $_POST['a_speaker_missed'];
-        $autoSpeakerTotal = $autoSpeakerScored + $autoSpeakerMissed;
-        $autoAmpScored = $_POST['a_amp_scored'];
-        $autoAmpMissed = $_POST['a_amp_missed'];
-        $autoAmpTotal = $autoAmpScored + $autoAmpMissed;
-        $teleSpeakerScored = $_POST['speaker_scored'];
-        $teleSpeakerMissed = $_POST['speaker_missed'];
-        $teleSpeakerTotal = $teleSpeakerScored + $teleSpeakerMissed;
-        $podiumSpeakerScored = $_POST['speaker_scored_protected'];
-        $podiumSpeakerMissed = $_POST['speaker_missed_protected'];
-        $podiumSpeakerTotal = $podiumSpeakerScored + $podiumSpeakerMissed;
-        $teleAmpScored = $_POST['amp_scored'];
-        $teleAmpMissed = $_POST['amp_missed'];
-        $teleAmpTotal = $teleAmpScored + $teleAmpMissed;
+        //Autonomous
+        $StartingZone = $_POST['auto_move'];
+        $AutoSpeakerScored = $_POST['a_speaker_scored'];
+        $AutoSpeakerMissed = $_POST['a_speaker_missed'];
+        $AutoSpeakerTotal = $AutoSpeakerScored + $AutoSpeakerMissed;
+        $AutoAmpScored = $_POST['a_amp_scored'];
+        $AutoAmpMissed = $_POST['a_amp_missed'];
+        $AutoAmpTotal = $AutoAmpScored + $AutoAmpMissed;
+        //Teleoperated
+        $SpeakerScored = $_POST['speaker_scored'];
+        $SpeakerMissed = $_POST['speaker_missed'];
+        $SpeakerTotal = $SpeakerScored + $SpeakerMissed;
+        $PodiumSpeakerScored = $_POST['speaker_scored_protected'];
+        $PodiumSpeakerMissed = $_POST['speaker_missed_protected'];
+        $PodiumTotal = $PodiumSpeakerScored + $PodiumSpeakerMissed;
+        $AmpScored = $_POST['amp_scored'];
+        $AmpMissed = $_POST['amp_missed'];
+        $AmpTotal = $AmpScored + $AmpMissed;
         $NotesDropped = $_POST['notes_dropped'];
+        //End Game
         $Climb = $_POST['climb'];
-        $trapNote = $_POST['trap_note'];
-        $DriveTeam = $_POST['drive_team'];
+        $TrapNote = $_POST['trap_note'];
+        $DriveTeamRating = $_POST['drive_team'];
         $Defense = $_POST['defense'];
         $DefenseComments = $_POST['defense_comments'];
         $DefenseRating = $_POST['defense_rating'];
@@ -35,21 +37,28 @@
         $Penalties = $_POST['penalties'];
         $Comments = $_POST['comments'];
 
-        $sql = "INSERT INTO MatchStats VALUES(NULL, $TeamNum, $CompID, $MatchNum, '$ScoutName', $startingZone, $autoSpeakerScored, $autoSpeakerMissed, $autoSpeakerTotal, $autoAmpScored, $autoAmpMissed, $autoAmpTotal,
-        $teleSpeakerScored, $teleSpeakerMissed, $teleSpeakerTotal, $podiumSpeakerScored, $podiumSpeakerMissed, $podiumSpeakerTotal, $teleAmpScored, $teleAmpMissed, $teleAmpTotal, $NotesDropped, '$Climb',
-        $trap_note, $DriveTeam, $Defense,'$DefenseComments', $DefenseRating, $IntakeRating, '$Penalties', '$Comments')";
-        if(mysqli_query($conn, $sql))
-        {
-            header('Location: ' . '../match_scout.php?CompID='. $CompID .'&submit=success');
+        $sql = "INSERT INTO MatchStats VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
+        ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $stmt = mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare($stmt, $sql)) {
+            header("Location: ../match_scout.php?CompID=$CompID'&error=sqlerror");
             exit();
+        } else {
+            mysqli_stmt_bind_param($stmt, "iiisiiiiiiiiiiiiiiiiisiiisiiss", $TeamNum, $CompID, $MatchNum,
+            $ScoutName, $StartingZone, $AutoSpeakerScored, $AutoSpeakerMissed, $AutoSpeakerTotal,
+            $AutoAmpScored, $AutoAmpMissed, $AutoAmpTotal, $SpeakerScored, $SpeakerMissed, $SpeakerTotal,
+            $PodiumSpeakerScored, $PodiumSpeakerMissed, $PodiumTotal, $AmpScored, $AmpMissed, $AmpTotal,
+            $NotesDropped, $Climb, $TrapNote, $DriveTeamRating, $Defense, $DefenseComments, $DefenseRating,
+            $IntakeRating, mysqli_real_escape_string($conn, $Penalties), mysqli_real_escape_string($conn, $Comments));
+            mysqli_stmt_execute($stmt);
         }
-        else
-        {
-            echo "ERROR: Could not execute $sql. " . mysqli_error($conn);
-        }
-    }
-    else
-    {
+
+        mysqli_close($conn);
+        header("Location: ../match_scout.php?CompID=$CompID&submit=success");
+        exit();
+
+    } else {
         header("Location: ../index.php");
         exit();
     }
+?>
